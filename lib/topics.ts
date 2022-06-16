a1e1be4cbc4a29ac34bcfe299feaa413a0243bf1
Bobby George <bobby.george@Blend360.com>
2022-06-16 09:25:26 -0400

    Remove all topics except all

--- lib/topics.ts
+++ lib/topics.ts
@@ -1,11 +1,5 @@
 const topics: { [key: string]: { label: string } } = {
   all: { label: "All" },
-  finance: { label: "Finance" },
-  economics: { label: "Economics" },
-  russia: { label: "Russia" },
-  Biden: { label: "Biden" },
-  education: { label: "Education" },
-  Depp: { label: "Depp" },
 };
 
 export default topics;
