2b27b85da3a282257fd6840789a10904868da0d5
Bobby George <slowtacocar@gmail.com>
2022-08-17 10:03:21 -0400

    Update grammar-check.ts

--- pages/api/grammar-check.ts
+++ pages/api/grammar-check.ts
@@ -1,10 +1,11 @@
 import nc from "next-connect";
 import { NextApiRequest, NextApiResponse } from "next";
-import { auth } from "../../lib/server/middleware";
+import { auth, onError, sentry } from "../../lib/server/middleware";
 import { invokeGrammarCheck } from "../../lib/server/lambda";
 import { GenerateResponse, GrammarCheckError } from "../../lib/types";
 
-export default nc<NextApiRequest, NextApiResponse>()
+export default nc<NextApiRequest, NextApiResponse>({ onError })
+  .use(sentry)
   .use(auth)
   .post(async (req, res: NextApiResponse<GenerateResponse | GrammarCheckError>) => {
     const body = await invokeGrammarCheck(req.body);
