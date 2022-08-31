53b26a5ce69c3fd738941ac3a96388a673a554c7
Charles <charles.du@blend360.com>
2022-08-31 09:13:43 -0400

    testing push into news-revelaer.com

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ const { App } = require("aws-cdk-lib");
 const PipelineStack = require("./pipeline");
 
 const app = new App();
-new PipelineStack(app, "newsrevealer-pipeline-dev", {
-  branch: "dev",
-  connection:
-    "arn:aws:codestar-connections:us-east-1:169196863399:connection/b8443c0f-0bc7-4918-8fa4-7d4158b67897",
-  domain: "dev.newsrevealer.blend360dev.io",
-  hostedZone: "Z0721626FLL4MCQSWB0S",
-  env: {
-    account: "169196863399",
-    region: "us-east-1",
-  },
-  stage: "dev",
-  environment: "dev",
-});
+//new PipelineStack(app, "newsrevealer-pipeline-dev", {
+//  branch: "dev",
+//  connection:
+//    "arn:aws:codestar-connections:us-east-1:169196863399:connection/b8443c0f-0bc7-4918-8fa4-7d4158b67897",
+//  domain: "dev.newsrevealer.blend360dev.io",
+//  hostedZone: "Z0721626FLL4MCQSWB0S",
+//  env: {
+//    account: "169196863399",
+//    region: "us-east-1",
+//  },
+//  stage: "dev",
+//  environment: "dev",
+//});
 //new PipelineStack(app, "newsrevealer-pipeline-stable", {
 //  branch: "main",
 //  connection:
