97a551372f1d76af63c6715a9b633526c6bcef15
Andrew Udvare <audvare@gmail.com>
2022-08-30 23:51:58 -0400

    comment to trigger build

--- app.js
+++ app.js
@@ -19,19 +19,6 @@ new PipelineStack(app, "newsrevealer-pipeline-stable", {
   branch: "main",
   connection:
     "arn:aws:codestar-connections:us-east-1:169196863399:connection/b8443c0f-0bc7-4918-8fa4-7d4158b67897",
-  domain: "newsrevealer.blend360dev.io",
-  hostedZone: "Z0721626FLL4MCQSWB0S",
-  env: {
-    account: "169196863399",
-    region: "us-east-1",
-  },
-  stage: "stable",
-  environment: "stable",
-});
-new PipelineStack(app, "newsrevealer-pipeline-prod", {
-  branch: "main",
-  connection:
-    "arn:aws:codestar-connections:us-east-1:169196863399:connection/b8443c0f-0bc7-4918-8fa4-7d4158b67897",
   domain: "news-revealer.io",
   hostedZone: "Z09265032BUJ9IJM9SA8U",
   env: {
@@ -41,4 +28,17 @@ new PipelineStack(app, "newsrevealer-pipeline-prod", {
   stage: "stable",
   environment: "stable",
 });
+// new PipelineStack(app, "newsrevealer-pipeline-prod", {
+//   branch: "main",
+//   connection:
+//     "arn:aws:codestar-connections:us-east-1:169196863399:connection/b8443c0f-0bc7-4918-8fa4-7d4158b67897",
+//   domain: "news-revealer.io",
+//   hostedZone: "Z09265032BUJ9IJM9SA8U",
+//   env: {
+//     account: "169196863399",
+//     region: "us-east-1",
+//   },
+//   stage: "stable",
+//   environment: "stable",
+// });
 // comment
