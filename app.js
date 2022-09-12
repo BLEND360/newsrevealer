431b2d49fb4ac04202efeb8e4f41c9559c1ed62d
Andrew Udvare <audvare@gmail.com>
2022-09-12 11:09:36 -0400

    Fix hosted zone ID

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ new PipelineStack(app, "newsrevealer-pipeline-stable", {
   connection:
     "arn:aws:codestar-connections:us-east-1:224306498215:connection/02d5e62d-6131-4197-9545-246edc80b55d",
   domain: "news-revealer.com",
-  hostedZone: "Z0721626FLL4MCQSWB0S",
+  hostedZone: "Z0782157UV9OHM12KTGY",
   env: {
     account: "224306498215",
     region: "us-east-1",
